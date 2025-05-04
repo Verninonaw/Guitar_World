d3e330e4a56bf465bf912a94573d140982295f68
Verninonaw <mprostopo4tam@gmail.com>
2025-05-04 12:09:25 +0300

    update

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId)
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth'})
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('header').addClass("dis");
+    } else {
+        $('header').removeClass("dis");
     }
-}
-
-function openOverlay() {
-    document.getElementById('overlay').style.display = 'block'
-}
-
-function closeOverlay() {
-    document.getElementById('overlay').style.display = 'none'
-}
-
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Спасибо за ваше сообщение! Мы скоро свяжемся с вами.');
-    this.reset();
-    closeOverlay();
 });
