482887b14aaf2c1ae931cd846bead5a60444bd0b
Verninonaw <mprostopo4tam@gmail.com>
2025-05-05 10:14:28 +0300

    #

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ $(window).scroll(function () {
     }
 });
 
+const name = document.querySelector('.name')
+const number = document.querySelector('.number')
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault()
+    if (name.value && number.value) {
+        alert('Заявка успешно оставлена')
+        name.value = '';
+        number.value = ''
+    }
+    else {
+        alert('Заполните все поля')
+    }
+})
